a06a6baa5d72fcbade804d5a4c6f7dd746727d83
BoobalanRP <116268868+BoobalanRP@users.noreply.github.com>
2023-07-11 12:03:55 +0530

    update day02 work

---
+++ src/main/java/day02/misc/Accademy.java
@@ -0,0 +1,32 @@
+package day02.misc;
+
+public class Accademy {
+	int id;
+	String code;
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String newName) {
+		newName = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int newId) {
+		newId = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String newCode) {
+		newCode = code;
+	}
+
+}
---
+++ src/main/java/day02/solved/Person.java
@@ -0,0 +1,10 @@
+package day02.solved;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
